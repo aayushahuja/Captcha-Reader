1c53f8534918923f400cae161e50c6a4c1a998cc
Aayush Ahuja <aayush.ahuja1991@gmail.com>
2011-12-17 19:31:31 +0530

    first commit

---
+++ Captcha/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Captcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 114);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.Filter = "JPG|*.jpg|BMP|*.bmp|PNG|*.png|GIF|*.gif";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Grayscale";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pre-Processing";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(389, 44);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(304, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(164, 84);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 198);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(113, 63);
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "croppedImages";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(261, 198);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(49, 63);
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(316, 198);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 63);
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(372, 198);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(47, 63);
+            this.pictureBox6.TabIndex = 10;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(425, 198);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(43, 63);
+            this.pictureBox7.TabIndex = 11;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "segments";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(101, 20);
+            this.textBox1.TabIndex = 13;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(131, 198);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(113, 63);
+            this.pictureBox8.TabIndex = 14;
+            this.pictureBox8.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 310);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(356, 310);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 414);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Captcha/Form1.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace Captcha
+{
+    public partial class Form1 : Form
+    {
+        Stack currentColour;
+        //ArrayList segments;
+        //Color currentColour;
+        Bitmap imgBitmap;
+        Image file;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static Bitmap crop(Bitmap img, int[] arr)
+        {
+            Bitmap croppedImage = new Bitmap(arr[2] - arr[0] + 1, arr[3] - arr[1] + 1);
+            for (int x = arr[0]; x <= arr[2]; x++)
+            {
+                for (int y = arr[1]; y <= arr[3]; y++)
+                {
+                    croppedImage.SetPixel(x - arr[0], y - arr[1], img.GetPixel(x, y));
+                }
+            }
+            return croppedImage;
+        }
+        private static Array extremes(Bitmap imgBitmap)
+        {
+            //finding xTopLeft
+            int xTopLeft = 0;
+            Boolean found = false;
+            for (int x = 0; x < imgBitmap.Width; x++)
+            {
+                for (int y = 0; y < imgBitmap.Height; y++)
+                {
+                    Color colour = imgBitmap.GetPixel(x, y);
+                    if (colour.GetBrightness() < .5)
+                    {
+                        found = true;
+                        break;
+                    }
+
+                }
+                if (found == true)
+                {
+                    break;
+                }
+                else
+                {
+                    xTopLeft++;
+                }
+            }
+
+            //finding yTopLeft
+            found = false;
+            int yTopLeft = 0;
+            for (int y = 0; y < imgBitmap.Height; y++)
+            {
+                for (int x = 0; x < imgBitmap.Width; x++)
+                {
+                    Color colour = imgBitmap.GetPixel(x, y);
+                    if (colour.GetBrightness() < .5)
+                    {
+                        found = true;
+                        break;
+                    }
+
+                }
+                if (found == true)
+                {
+                    break;
+                }
+                else
+                {
+                    yTopLeft++;
+                }
+            }
+
+            //finding xBottomRight
+            int xBottomRight = imgBitmap.Width - 1;
+            found = false;
+            for (int x = imgBitmap.Width - 1; x >= 0; x--)
+            {
+                for (int y = imgBitmap.Height - 1; y >= 0; y--)
+                {
+                    Color colour = imgBitmap.GetPixel(x, y);
+                    if (colour.GetBrightness() < .5)
+                    {
+                        found = true;
+                        break;
+                    }
+
+                }
+                if (found == true)
+                {
+                    break;
+                }
+                else
+                {
+                    xBottomRight--;
+                }
+            }
+            //finding yBottomRight
+            int yBottomRight = imgBitmap.Height - 1;
+            found = false;
+            for (int y = imgBitmap.Height - 1; y >= 0; y--)
+            {
+                for (int x = imgBitmap.Width - 1; x >= 0; x--)
+                {
+                    Color colour = imgBitmap.GetPixel(x, y);
+                    if (colour.GetBrightness() < .5)
+                    {
+                        found = true;
+                        break;
+                    }
+
+                }
+                if (found == true)
+                {
+                    break;
+                }
+                else
+                {
+                    yBottomRight--;
+                }
+            }
+            int[] arr = new int[4] ;
+            arr[0] = xTopLeft;
+            arr[1] = yTopLeft;
+            arr[2] = xBottomRight;
+            arr[3] = yBottomRight;
+            return arr;
+        }
+        
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                imgBitmap = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = file;
+            }
+        }
+
+        private static Bitmap makeOthersWhite(Bitmap img,Color colour){
+            Bitmap a = new Bitmap(img);
+            for (int x = 0; x < img.Width; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    if (img.GetPixel(x, y) != colour)
+                    {
+                        a.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                    }
+                }
+            }
+                return a;
+        }
+        private static Bitmap crop(int startIndex, int endIndex,Bitmap img)
+        {
+            
+            Bitmap a = new Bitmap(endIndex - startIndex + 1, img.Height);
+            for (int x = startIndex; x <= endIndex; x++)
+            {
+                for (int y = 0; y < img.Height; y++)
+                {
+                    a.SetPixel(x - startIndex,y,img.GetPixel(x, y));
+                }
+            }
+            return a;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+            //currentColour = Color.FromArgb(255,255,255);
+            currentColour = new Stack();
+            //segments.Add(imgBitmap);
+               
+            //removing noise
+            for (int x = 0; x < imgBitmap.Width; x++)
+            {
+                for (int y = 0; y < imgBitmap.Height; y++)
+                {
+                    Color colour = imgBitmap.GetPixel(x, y);
+                    if (colour.GetBrightness() > .5)
+                    {
+                        imgBitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                    }
+                    else
+                    {
+                        imgBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    }
+                   
+                }
+            }
+            pictureBox2.Image = imgBitmap;
+            checkBox1.Checked = true;
+
+            //narowing down the search
+            int[] croppedImgExtremes = (int[])extremes(imgBitmap);
+            //got (xtopleft,yTopLeft) and (xBottomRight,yBottomRight)
+            Bitmap croppedImage = crop(imgBitmap, croppedImgExtremes);
+            //got cropped image
+            pictureBox3.Image = croppedImage;
+
+            //removing white regions
+            ArrayList onlyWhite = new ArrayList();
+            ArrayList endPoints = new ArrayList();
+            bool flag = false;
+            for (int x = 0; x < croppedImage.Width; x++)
+            {
+                flag = true;
+                for (int y = 0; y < croppedImage.Height; y++)
+                {
+                    if (croppedImage.GetPixel(x, y) == Color.FromArgb(0, 0, 0))
+                    {
+                        flag = false;
+                    }
+                }
+                if (flag == true)
+                {
+                    onlyWhite.Add(x);
+                    
+                    //adding one extra line
+                    if (onlyWhite.Count > 1)
+                    {
+                        if ((int)onlyWhite[onlyWhite.Count - 1] != (int)((int)onlyWhite[onlyWhite.Count - 2] + 1))
+                        {
+                            endPoints.Add(onlyWhite[onlyWhite.Count - 2]);
+                            endPoints.Add(onlyWhite[onlyWhite.Count - 1]);
+
+                        }
+                    }
+                }
+            }
+            endPoints.Insert(0, onlyWhite[0]);
+            endPoints.Add(onlyWhite[onlyWhite.Count - 1]);
+            
+            /*
+            for (int i = 1; i < onlyWhite.Count; i++)
+            {
+                if ((int)onlyWhite[i] == ((int)onlyWhite[(i - 1)] + 1)) { continue; }
+                else { endPoints.Add(onlyWhite[i-1]); onlyWhite.RemoveAt(i - 1); i--; }
+            }
+            */
+            int newWidth = croppedImage.Width - onlyWhite.Count;
+
+            //cropping again
+            Bitmap croppedImage2 = new Bitmap(newWidth, croppedImage.Height);
+            int x2=0;
+            for (int x = 0; x < croppedImage.Width;x++ )
+            {
+                if (onlyWhite.Contains(x))
+                {
+                    continue;
+                }
+                else
+                {
+                    for (int y = 0; y < croppedImage2.Height; y++)
+                    {
+                        croppedImage2.SetPixel(x2, y, croppedImage.GetPixel(x, y));
+                    }
+                    x2++;
+                }
+            }
+            pictureBox8.Image = croppedImage2;
+            pictureBox4.Image = null;
+            pictureBox5.Image = null;
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            textBox1.Text = ((int)endPoints[0]).ToString();
+            textBox2.Text = endPoints[1].ToString();
+           // textBox3.Text = endPoints[2].ToString();
+            Bitmap dummy;
+            dummy=crop(0, (int)(endPoints[0]), croppedImage);
+            dummy = crop(dummy, (int[])extremes(dummy));
+            pictureBox4.Image = dummy;
+
+            if (endPoints.Count > 2)
+            {
+                dummy=crop((int)endPoints[1], (int)endPoints[2], croppedImage);
+                dummy = crop(dummy, (int[])extremes(dummy));
+                pictureBox5.Image = dummy;
+                if (endPoints.Count <= 4)
+                {
+                    dummy = crop((int)endPoints[3], ((int)(croppedImage.Width) - 1), croppedImage);
+                    dummy = crop(dummy, (int[])extremes(dummy));
+                    pictureBox6.Image = dummy;
+                }
+                else
+                {
+                    dummy = crop((int)endPoints[3], (int)endPoints[4], croppedImage);
+                    dummy = crop(dummy, (int[])extremes(dummy));
+                    pictureBox6.Image = dummy;
+                    dummy = crop((int)endPoints[5], ((int)(croppedImage.Width) - 1), croppedImage);
+                    dummy = crop(dummy, (int[])extremes(dummy));
+                    pictureBox7.Image = dummy;
+                }
+            }
+            else
+            {
+                dummy=crop((int)endPoints[1], ((int)(croppedImage.Width) - 1), croppedImage);
+                dummy = crop(dummy, (int[])extremes(dummy));
+                pictureBox5.Image = dummy;
+            }
+            
+            /*
+            int end1 = segmentXIndex(croppedImage2, 0);
+            int end2 = segmentXIndex(croppedImage2, end1);
+            if(end2<croppedImage2.Width){
+                int end3 = segmentXIndex(croppedImage2,end2);
+            }*/
+            /*Color[] doneColours = new Color[5];
+            int countDoneColours = 0;
+            Bitmap a;
+            Bitmap[] segments = new Bitmap[5];
+            bool contain = false;
+            int[] b;
+            doneColours[0] = Color.FromArgb(23, 23, 23);
+            Color ac = Color.FromArgb(23, 23, 23);
+            Color colours;
+            /*if (doneColours[0] == ac)
+            {
+                textBox1.Text = croppedImage.GetPixel(23,23);
+            }*/
+                //segmenting parts take one colour make others zero then distance between extremes do
+            /*   for (int x = 0; x < croppedImage.Width; x++)
+                {
+                    for (int y = 0; y < croppedImage.Height; y++)
+                    {
+                        colours = croppedImage.GetPixel(x, y);
+                        contain = false;
+                        for (int i = 0; i < countDoneColours; i++)
+                        {
+                            if (doneColours[i]==colours)
+                            {
+                              //  Environment.Exit(0);
+                                contain = true;
+                                break;
+                            }
+                        }
+                            if ((colours.GetBrightness() < .5) && contain==false)
+                            {
+                                a = makeOthersWhite(croppedImage, colours);
+                                b = (int[])extremes(a);
+                                pictureBox4.Image = crop(croppedImage, b);
+                                //Environment.Exit(0);
+                                /*if ((b[2] - b[0]) > ((croppedImage.Width) / 2.8))
+                                {
+
+                                }
+                                else {
+                                    segments.Add(crop(croppedImage, b));
+
+                                }*/
+            
+              /*                  textBox1.Text = countDoneColours.ToString();
+                                pictureBox4.Image = crop(croppedImage, b);
+                              //  segments[countDoneColours] = crop(croppedImage, b);
+                               // doneColours[countDoneColours] = colours;
+                                countDoneColours++;
+                                if (countDoneColours > 3) { //Environment.Exit(0); 
+                                }
+                            }
+                    }
+                }
+            //displaying the segments
+            /*   pictureBox4.Image = segments[0];
+               pictureBox5.Image = segments[1];
+               pictureBox6.Image = segments[2];
+            if(countSeg==3){
+                pictureBox7.Image = segments[3];
+            } 
+            */
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
